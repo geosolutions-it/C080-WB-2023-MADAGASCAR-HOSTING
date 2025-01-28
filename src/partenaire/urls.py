2f2b2e8c8a2b57f4a628c3489b047c413c5cf335
Mattia <mattia.giupponi@gmail.com>
2025-01-28 16:41:02 +0100

    fix docker-compose file

--- src/partenaire/urls.py
+++ src/partenaire/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
+
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 from . import views
 
 
 urlpatterns = [
-   url(r'^$',
+   re_path(r'^$',
        TemplateView.as_view(template_name='partenaire/partenaire_list.html'),
        name='partenaire_list'),
 ]
